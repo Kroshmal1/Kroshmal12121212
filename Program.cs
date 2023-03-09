d73762d02d37a57eaadcddb758180691a62aff62
Kroshmal1 <127083651+Kroshmal1@users.noreply.github.com>
2023-03-09 11:15:34 +0200

    1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Крошмаль09._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6];
+            for (int i=0;i<6;i++)
+            {
+                Console.Write($"array[{i}]=");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"array[{i}])= {array[i]}");
+            }
+            Console.Read();
+        }
+    }
+}
